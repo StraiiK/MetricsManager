cd07cf698918feb9a87e65a78e212ce5d9b90fdf
StraiK <nanostraik@gmail.com>
2023-08-10 17:35:09 +0300

    Создал проект, обьект WeatherMetrics, репозиторий TemperatureRepo, контроллер WeatherMetricsController

---
+++ MetricsManager/Controllers/WeatherMetricsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace MetricsManager
+{
+    [Route("api/WeatherMetrics")]
+    [ApiController]
+    public class WeatherMetricsController : ControllerBase
+    {
+        private TemperatureRepo _temperatureRepo;
+
+        public WeatherMetricsController(TemperatureRepo temperatureRepo)
+        {
+            _temperatureRepo = temperatureRepo;
+        }
+
+        [HttpGet]
+        public IActionResult GetTemperaturesAll()
+        {
+            var temperatures = _temperatureRepo.GetTemperaturesAll();
+            if (temperatures.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(temperatures);
+        }
+
+        [HttpGet]
+        public IActionResult GetTemperatures(DateTime date, DateTime endTime)
+        {
+            var temperatures = _temperatureRepo.GetTemperatures(date, endTime);
+            if (temperatures.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(temperatures);
+        }
+
+        [HttpPost]
+        public IActionResult SaveTemperature([FromQuery] DateTime timeStamp, [FromQuery] double temperature)
+        {
+            _temperatureRepo.SaveTemperature(timeStamp, temperature);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult EditTemperature([FromQuery] DateTime timeStamp, [FromQuery] double newTemperature)
+        {
+            _temperatureRepo.EditTemperature(timeStamp, newTemperature);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(DateTime date, DateTime endTime)
+        {
+            _temperatureRepo.DeleteTemperatures(date, endTime);
+            return Ok();
+        }
+    }
+}
---
+++ MetricsManager/WeatherMetrics.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace MetricsManager
+{
+    public class WeatherMetrics
+    {
+        public DateTime Date { get; set; }
+        public Double Temperature { get; set; }
+    }
+}
